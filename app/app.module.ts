137f324c82310c844a9ac96faafb8b3a91f6617e
Nitin <nitinprakash@hotmail.com>
2017-03-23 15:53:31 -0400

    First Commit

---
+++ app/app.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
+import {FormsModule} from '@angular/forms';
+
+import { AppComponent }  from './app.component';
+import { WelcomeComponent } from './home/welcome.component';
+
+/* Feature Modules */
+import { ProductModule } from './products/product.module';
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    RouterModule.forRoot([
+      { path: 'welcome', component: WelcomeComponent },
+      { path: '', redirectTo: 'welcome', pathMatch: 'full' },
+      { path: '**', redirectTo: 'welcome', pathMatch: 'full' }
+    ]),
+    ProductModule
+  ],
+  declarations: [
+    AppComponent,
+    WelcomeComponent
+  ],
+  bootstrap: [ AppComponent ]
+})
+export class AppModule { }
